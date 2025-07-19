0f3a1ca129faaba9644557fb3b6926e3ec25aedd
gecarval <gersonrcarvalho//@gmail.com>
2025-07-19 22:53:18 +0100

    fix(quads): rendering and quadsize calculation.

--- classes/QuadTree.cpp
+++ classes/QuadTree.cpp
@@ -2,170 +2,167 @@
 #include <cstdlib>
 
 QuadTree::QuadTree()
-	: tmass(0), qPos(PARENT), level(0), count(0), empty(true), divided(false),
-	  parent(nullptr), nw(nullptr), ne(nullptr), sw(nullptr), se(nullptr) {
-	this->min = {0, 0};
-	this->max = {1920, 1080};
-	const float width = (std::abs(this->max.x)) + (this->min.x);
-	const float height = (std::abs(this->max.y)) + (this->min.y);
-	this->quad = (Rectangle){this->min.x, this->min.y, width, height};
-};
+    : quad(Rectangle()), tmass(0), min(Vector2()), max(Vector2()), qPos(PARENT),
+      level(0), count(0), empty(true), divided(false), parent(nullptr),
+      nw(nullptr), ne(nullptr), sw(nullptr), se(nullptr) {};
 
 QuadTree::QuadTree(QuadTree *p, const Rectangle &q, const size_t &lvl,
-				   const uint8_t &qp)
-	: quad(q), tmass(0), qPos(qp), level(lvl), count(0), empty(true),
-	  divided(false), parent(p), nw(nullptr), ne(nullptr), sw(nullptr),
-	  se(nullptr) {};
+                   const uint8_t &qp)
+    : quad(q), tmass(0), qPos(qp), level(lvl), count(0), empty(true),
+      divided(false), parent(p), nw(nullptr), ne(nullptr), sw(nullptr),
+      se(nullptr) {};
 
 static void clearChildren(QuadTree *t) {
-	if (t == nullptr) return;
-	if (t->isDivided()) {
-		clearChildren(t->getChildren(NORTH_WEST));
-		clearChildren(t->getChildren(NORTH_EAST));
-		clearChildren(t->getChildren(SOUTH_WEST));
-		clearChildren(t->getChildren(SOUTH_EAST));
-	}
-	if (t->getLevel() != 0) {
-		delete t;
-		return;
-	}
+  if (t == nullptr)
+    return;
+  if (t->isDivided()) {
+    clearChildren(t->getChildren(NORTH_WEST));
+    clearChildren(t->getChildren(NORTH_EAST));
+    clearChildren(t->getChildren(SOUTH_WEST));
+    clearChildren(t->getChildren(SOUTH_EAST));
+  }
+  if (t->getLevel() != 0)
+    delete t;
 };
 
 QuadTree::~QuadTree() {};
 
 void QuadTree::build(std::deque<Particle> &p) {
-	const float width = (std::abs(this->max.x)) + (this->min.x);
-	const float height = (std::abs(this->max.y)) + (this->min.y);
-	this->quad = (Rectangle){this->min.x, this->min.y, width, height};
-	for (std::deque<Particle>::iterator p1 = p.begin(); p1 != p.cend(); p1++) {
-		this->insert(&(*p1));
-	}
+  const Vector2 pos = (Vector2){this->min.x, this->min.y};
+  const float width = std::abs(this->max.x) + std::abs(this->min.x);
+  const float height = std::abs(this->max.y) + std::abs(this->min.y);
+  this->quad = (Rectangle){pos.x, pos.y, width, height};
+  for (std::deque<Particle>::iterator p1 = p.begin(); p1 != p.cend(); p1++) {
+    this->insert(&(*p1));
+  }
 };
 
 void QuadTree::subDivide(void) {
-	Rectangle	r;
-	const float cx = this->quad.x;
-	const float cy = this->quad.y;
-	const float wd = this->quad.width / 2.0f;
-	const float hg = this->quad.height / 2.0f;
-	this->divided = true;
-	r = (Rectangle){cx, cy, wd, hg};
-	this->nw = new QuadTree(this, r, this->level + 1, NORTH_WEST);
-	r = (Rectangle){cx + wd, cy, wd, hg};
-	this->ne = new QuadTree(this, r, this->level + 1, NORTH_EAST);
-	r = (Rectangle){cx, cy + hg, wd, hg};
-	this->sw = new QuadTree(this, r, this->level + 1, SOUTH_WEST);
-	r = (Rectangle){cx + wd, cy + hg, wd, hg};
-	this->se = new QuadTree(this, r, this->level + 1, SOUTH_EAST);
-	for (size_t i = 0; i < MAX_CAPACITY; i++) {
-		this->nw->insert(p[i]);
-		this->ne->insert(p[i]);
-		this->sw->insert(p[i]);
-		this->se->insert(p[i]);
-		this->p[i] = nullptr;
-	};
+  Rectangle r;
+  const float cx = this->quad.x;
+  const float cy = this->quad.y;
+  const float wd = this->quad.width / 2.0f;
+  const float hg = this->quad.height / 2.0f;
+  this->divided = true;
+  r = (Rectangle){cx, cy, wd, hg};
+  this->nw = new QuadTree(this, r, this->level + 1, NORTH_WEST);
+  r = (Rectangle){cx + wd, cy, wd, hg};
+  this->ne = new QuadTree(this, r, this->level + 1, NORTH_EAST);
+  r = (Rectangle){cx, cy + hg, wd, hg};
+  this->sw = new QuadTree(this, r, this->level + 1, SOUTH_WEST);
+  r = (Rectangle){cx + wd, cy + hg, wd, hg};
+  this->se = new QuadTree(this, r, this->level + 1, SOUTH_EAST);
+  for (size_t i = 0; i < MAX_CAPACITY; i++) {
+    this->nw->insert(p[i]);
+    this->ne->insert(p[i]);
+    this->sw->insert(p[i]);
+    this->se->insert(p[i]);
+    this->p[i] = nullptr;
+  };
 }
 
 void QuadTree::insert(Particle *p) {
-	if (!CheckCollisionPointRec(p->getPos(), this->quad)) return;
-	this->mid += p->getPos();
-	this->tmass += p->getMass();
-	if (this->isDivided()) {
-		this->count++;
-		this->nw->insert(p);
-		this->ne->insert(p);
-		this->sw->insert(p);
-		this->se->insert(p);
-		return;
-	}
-	if (this->count < MAX_CAPACITY) {
-		this->empty = false;
-		this->p[this->count++] = p;
-		return;
-	}
-	this->subDivide();
-	this->nw->insert(p);
-	this->ne->insert(p);
-	this->sw->insert(p);
-	this->se->insert(p);
+  if (!CheckCollisionPointRec(p->getPos(), this->quad))
+    return;
+  this->mid += p->getPos();
+  this->tmass += p->getMass();
+  if (this->isDivided()) {
+    this->count++;
+    this->nw->insert(p);
+    this->ne->insert(p);
+    this->sw->insert(p);
+    this->se->insert(p);
+    return;
+  }
+  if (this->count < MAX_CAPACITY) {
+    this->empty = false;
+    this->p[this->count++] = p;
+    return;
+  }
+  this->subDivide();
+  this->nw->insert(p);
+  this->ne->insert(p);
+  this->sw->insert(p);
+  this->se->insert(p);
 };
 
 void QuadTree::clear(void) {
-	this->tmass = 0.0;
-	this->qPos = PARENT;
-	this->level = 0;
-	this->count = 0;
-	this->empty = true;
-	this->divided = false;
-	this->parent = nullptr;
-	clearChildren(this);
-	this->nw = nullptr;
-	this->ne = nullptr;
-	this->sw = nullptr;
-	this->se = nullptr;
+  this->tmass = 0.0;
+  this->qPos = PARENT;
+  this->level = 0;
+  this->count = 0;
+  this->empty = true;
+  this->divided = false;
+  this->parent = nullptr;
+  clearChildren(this);
+  this->nw = nullptr;
+  this->ne = nullptr;
+  this->sw = nullptr;
+  this->se = nullptr;
 };
 
 void QuadTree::renderQuads(const Camera2D &c) {
-	if (this->isDivided()) {
-		this->nw->renderQuads(c);
-		this->ne->renderQuads(c);
-		this->sw->renderQuads(c);
-		this->se->renderQuads(c);
-	}
-	const float		posX = (this->quad.x - c.target.x) * c.zoom + c.offset.x;
-	const float		posY = (c.target.y - this->quad.y) * c.zoom + c.offset.y;
-	const float		width = this->quad.width * c.zoom;
-	const float		height = this->quad.height * c.zoom;
-	const float		lineThickness = 1.0f;
-	const Rectangle rec = (Rectangle){posX, posY, width, height};
-	DrawRectangleLinesEx(rec, lineThickness, RED);
+  if (this->isDivided()) {
+    this->nw->renderQuads(c);
+    this->ne->renderQuads(c);
+    this->sw->renderQuads(c);
+    this->se->renderQuads(c);
+  }
+  // const float posX = (this->quad.x - c.target.x) * c.zoom + c.offset.x;
+  // const float posY = (-this->quad.y + c.target.y) * c.zoom + c.offset.y;
+  // const float width = this->quad.width * c.zoom;
+  // const float height = this->quad.height * c.zoom;
+  // const Rectangle rec = (Rectangle){posX, posY, width, height};
+  const float lineThickness = 1.0f;
+  const Rectangle rec = this->quad;
+  DrawRectangleLinesEx(rec, lineThickness, RED);
 };
 
-void QuadTree::setLevel(const size_t &lvl) {
-	this->level = lvl;
-};
+void QuadTree::setLevel(const size_t &lvl) { this->level = lvl; };
 
-const size_t &QuadTree::getLevel(void) const {
-	return (this->level);
-};
+const size_t &QuadTree::getLevel(void) const { return (this->level); };
 
 void QuadTree::setMax(const Vector2 &v) {
-	if (v.x > this->max.x) this->max.x = v.x;
-	if (v.y > this->max.y) this->max.y = v.y;
+  if (v.x > this->max.x)
+    this->max.x = v.x;
+  if (v.y > this->max.y)
+    this->max.y = v.y;
 };
 
-const Vector2 &QuadTree::getMax(void) const {
-	return (this->max);
-};
+const Vector2 &QuadTree::getMax(void) const { return (this->max); };
 
 void QuadTree::setMin(const Vector2 &v) {
-	if (v.x < this->min.x) this->min.x = v.x;
-	if (v.y < this->min.y) this->min.y = v.y;
+  if (v.x < this->min.x)
+    this->min.x = v.x;
+  if (v.y < this->min.y)
+    this->min.y = v.y;
 };
 
-const Vector2 &QuadTree::getMin(void) const {
-	return (this->min);
-};
+const Vector2 &QuadTree::getMin(void) const { return (this->min); };
 
-void QuadTree::isDivided(const bool &state) {
-	this->divided = state;
-};
+void QuadTree::isDivided(const bool &state) { this->divided = state; };
 
-const bool &QuadTree::isDivided(void) const {
-	return (this->divided);
-};
+const bool &QuadTree::isDivided(void) const { return (this->divided); };
 
 void QuadTree::setChildren(QuadTree *node, const uint8_t &n) {
-	if (n & NORTH_WEST) this->nw = node;
-	if (n & NORTH_EAST) this->ne = node;
-	if (n & SOUTH_WEST) this->sw = node;
-	if (n & SOUTH_EAST) this->se = node;
+  if (n & NORTH_WEST)
+    this->nw = node;
+  if (n & NORTH_EAST)
+    this->ne = node;
+  if (n & SOUTH_WEST)
+    this->sw = node;
+  if (n & SOUTH_EAST)
+    this->se = node;
 };
 
 QuadTree *QuadTree::getChildren(const uint8_t &n) const {
-	if (n & NORTH_WEST) return (this->nw);
-	if (n & NORTH_EAST) return (this->ne);
-	if (n & SOUTH_WEST) return (this->sw);
-	if (n & SOUTH_EAST) return (this->se);
-	return (nullptr);
+  if (n & NORTH_WEST)
+    return (this->nw);
+  if (n & NORTH_EAST)
+    return (this->ne);
+  if (n & SOUTH_WEST)
+    return (this->sw);
+  if (n & SOUTH_EAST)
+    return (this->se);
+  return (nullptr);
 };
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,99 +2,101 @@
 #include <deque>
 
 // Window Settings
-static const char	*windowTitle = "Gravity Sand";
-static const int	 windowWidth = 1600;
-static const int	 windowheight = 900;
-static const int	 frameLimit = 120;
+static const char *windowTitle = "Gravity Sand";
+static const int windowWidth = 1200;
+static const int windowheight = 800;
+static const int frameLimit = 120;
 static const Vector2 drawFpsPos = (Vector2){10.0f, 10.0f};
 
 // Game Camera2D Settings
-static const float	 posX = windowWidth / 2.0f;
-static const float	 posY = windowheight / 2.0f;
+static const float posX = windowWidth / 2.0f;
+static const float posY = windowheight / 2.0f;
 static const Vector2 screenMiddle = (Vector2){posX, posY};
 static const Vector2 target = screenMiddle;
 static const Vector2 offset = screenMiddle;
-static const float	 rotation = 0.0f;
-static const float	 zoom = 1.0f;
+static const float rotation = 0.0f;
+static const float zoom = 1.0f;
 
 // Render Texture Settings
 static const Vector2 axisCenter = (Vector2){0.0f, 0.0f};
-static const Color	 backGroundColor = (Color){0, 0, 24, 255};
-static const Color	 screenColorTint = WHITE;
+static const Color backGroundColor = (Color){0, 0, 24, 255};
+static const Color screenColorTint = WHITE;
 
 // Simulation Settings
-static const int   particleMaxAmount = 500;
+static const int particleMaxAmount = 500;
 static const float defaultGravityValue = 3000.0f;
 
 void initParticles(Game &game) {
-	float	randYPos;
-	float	randXPos;
-	Vector2 pos;
+  float randYPos;
+  float randXPos;
+  Vector2 pos;
 
-	game.particles = std::deque<Particle>(particleMaxAmount);
-	for (std::deque<Particle>::iterator p = game.particles.begin();
-		 p != game.particles.end(); p++) {
-		randYPos = rand() % windowheight;
-		randXPos = rand() % windowWidth;
-		pos = (Vector2){randXPos, randYPos};
-		(*p).setNewType(rand() % 3);
-		(*p).setPos(pos);
-	}
+  game.particles = std::deque<Particle>(particleMaxAmount);
+  for (std::deque<Particle>::iterator p = game.particles.begin();
+       p != game.particles.end(); p++) {
+    randXPos = rand() % windowWidth;
+    randYPos = rand() % windowheight;
+    pos = (Vector2){randXPos, randYPos};
+    (*p).setPos(pos);
+    (*p).setNewType(rand() % ParticleTypeLen);
+    game.quadTree.setMin(pos);
+    game.quadTree.setMax(pos);
+  }
 }
 
 void initEngine(Game &game) {
-	InitWindow(windowWidth, windowheight, windowTitle);
-	SetTargetFPS(frameLimit);
-	rlImGuiSetup(true);
-	initParticles(game);
-	game.gravity.setGravity(defaultGravityValue);
-	game.camera = (Camera2D){offset, target, rotation, zoom};
-	game.screen = LoadRenderTexture(windowWidth, windowheight);
+  InitWindow(windowWidth, windowheight, windowTitle);
+  SetTargetFPS(frameLimit);
+  rlImGuiSetup(true);
+  initParticles(game);
+  game.gravity.setGravity(defaultGravityValue);
+  game.camera = (Camera2D){offset, target, rotation, zoom};
+  game.screen = LoadRenderTexture(windowWidth, windowheight);
 }
 
 void renderGame(Game &game) {
-	BeginTextureMode(game.screen);
-	ClearBackground(backGroundColor);
-	for (std::deque<Particle>::iterator p = game.particles.begin();
-		 p != game.particles.cend(); p++) {
-		(*p).renderAt(game.camera);
-		game.quadTree.setMin((*p).getPos());
-		game.quadTree.setMax((*p).getPos());
-	}
-	game.quadTree.renderQuads(game.camera);
-	EndTextureMode();
+  BeginTextureMode(game.screen);
+  ClearBackground(backGroundColor);
+  for (std::deque<Particle>::iterator p = game.particles.begin();
+       p != game.particles.cend(); p++) {
+    (*p).renderAt(game.camera);
+    game.quadTree.setMin((*p).getPos());
+    game.quadTree.setMax((*p).getPos());
+  }
+  EndTextureMode();
+  BeginDrawing();
+  DrawTexture(game.screen.texture, axisCenter.x, axisCenter.y, screenColorTint);
+  BeginMode2D(game.camera);
+  game.quadTree.renderQuads(game.camera);
+  EndMode2D();
+  DrawFPS(drawFpsPos.x, drawFpsPos.y);
 }
 
 void updateEngine(Game &game) {
-	while (!WindowShouldClose()) {
-		engineInput(game);
-		game.gravity.attract(game.particles);
-		game.updateParticlesVectors();
-		renderGame(game);
-		BeginDrawing();
-		DrawTexture(game.screen.texture, axisCenter.x, axisCenter.y,
-					screenColorTint);
-		BeginMode2D(game.camera);
-		EndMode2D();
-		DrawFPS(drawFpsPos.x, drawFpsPos.y);
-		renderImGui(game);
-		EndDrawing();
-		// game.quadTree.clear();
-		// game.quadTree.build(game.particles);
-	}
+  while (!WindowShouldClose()) {
+    engineInput(game);
+    game.quadTree.build(game.particles);
+    game.gravity.attract(game.particles);
+    game.updateParticlesVectors();
+    renderGame(game);
+    renderImGui(game);
+    EndDrawing();
+    game.quadTree.clear();
+  }
 }
 
 void endEngine(Game &game) {
-	// game.quadTree.clear();
-	(void)game;
-	rlImGuiShutdown();
-	CloseWindow();
+  game.quadTree.clear();
+  game.particles.clear();
+  UnloadTexture(game.screen.texture);
+  rlImGuiShutdown();
+  CloseWindow();
 }
 
 int main(void) {
-	Game game;
-	initEngine(game);
-	updateEngine(game);
-	endEngine(game);
-	return (0);
+  Game game;
+  initEngine(game);
+  updateEngine(game);
+  endEngine(game);
+  return (0);
 }
